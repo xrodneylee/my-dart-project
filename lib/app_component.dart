1caef12085121ed439be8f9bb05f67799c4975f9
Lee Guan Pu <xrodneylee@infinitiessoft.com>
2017-07-21 12:54:30 +0800

    update

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,6 +1,7 @@
 // Copyright (c) 2017, Eric. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
+import 'dart:async';
 import 'dart:html';
 
 import 'package:Whats_for_lunch/dto/store.dart';
@@ -15,6 +16,18 @@ import 'package:dartson/dartson.dart';
   styleUrls: const ['app_component.css'],
   templateUrl: 'app_component.html'
 )
-class AppComponent {
+class AppComponent implements OnInit {
   // Nothing here yet. All logic is in TodoListComponent.
+  @override
+  Future ngOnInit() async {
+    // TODO: implement ngOnInit
+    Dartson _dson = new Dartson.JSON();
+    List<Store> stores = [];
+    String result = await HttpRequest.getString('stores.json');
+    stores = _dson.decode(result, new Store(), true);
+    stores.forEach((store) {
+      print(store.name);
+    });
+    print(stores.length);
+  }
 }
